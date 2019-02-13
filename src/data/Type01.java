85716ae05ad5e79e5de04b775f551179ec7105e8
Administrator <Administrator@USER-PC>
2019-02-13 13:13:59 +0900

    UTF-8 변경후 커밋

--- src/data/Type01.java
+++ src/data/Type01.java
@@ -3,10 +3,10 @@ package data;
 public class Type01 {
 
 	public static void main(String[] args) {
-		// ¹®ÀÚ
+		// ë¬¸ì
 		char c = 'a';
 		
-		// ¼ıÀÚ - Á¤¼ö
+		// ìˆ«ì - ì •ìˆ˜
 		byte b = 127;
 		b = -128;
 		
@@ -16,12 +16,12 @@ public class Type01 {
 		
 		long l = 10000000000l;
 		
-		// ¼ıÀÚ - ½Ç¼ö
+		// ìˆ«ì - ì‹¤ìˆ˜
 		float f = 1.2f;
 		
 		double db = 1.2;
 		
-		// ³í¸®Çü(ºÒ¸°)
+		// ë…¼ë¦¬í˜•(ë¶ˆë¦°)
 		boolean bl = true;
 	}
 }
